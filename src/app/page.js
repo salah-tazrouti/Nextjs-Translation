6c9e0d7d4c57c8875a19754a28c57c670a5d6574
stazrouti <TAZROUTI.SALAHEDDINE@ofppt-edu.ma>
2024-01-15 17:54:38 +0100

    add redirection

--- src/app/page.js
+++ src/app/page.js
@@ -1,60 +1,20 @@
 "use client";
 import Link from 'next/link';
 import './globals.css';
+/* import LanguageSwitcher from './LanguageSwitcher'; */
+import { useTranslation } from 'react-i18next';
+import i18next from 'i18next';
+import { Layout } from './DefaultLayout';
+import { redirect } from 'next/navigation';
+import { Lang } from './Currentlng';
+
 export default function Home() {
+
+  const { t } = useTranslation();
+  //console.log(i18next.changeLanguage(lang));
   return (
-    <div className="p-8">
-      <p className="text-2xl font-bold mb-4">Blogs using files name</p>
-
-      <div className="mb-4">
-        <Link href="/Blogs/Blog1">
-          <div className="text-blue-500 hover:underline cursor-pointer">
-            Go to Blog1
-          </div>
-        </Link>
-      </div>
-
-      <div className="mb-4">
-        <Link href="/Blogs/Blog2">
-          <div className="text-blue-500 hover:underline cursor-pointer">
-            Go to Blog2
-          </div>
-        </Link>
-      </div>
-
-      <div className="mb-4">
-        <Link href="/Blogs/Blog3">
-          <div className="text-blue-500 hover:underline cursor-pointer">
-            Go to Blog3
-          </div>
-        </Link>
-      </div>
-
-      <p className="text-2xl font-bold my-4">Blogs using slugs name</p>
-
-      <div className="mb-4">
-        <Link href="/posts/Art">
-          <div className="text-blue-500 hover:underline cursor-pointer">
-            Go to Art
-          </div>
-        </Link>
-      </div>
-
-      <div className="mb-4">
-        <Link href="/posts/It">
-          <div className="text-blue-500 hover:underline cursor-pointer">
-            Go to It
-          </div>
-        </Link>
-      </div>
-
-      <div className="mb-4">
-        <Link href="/posts/World">
-          <div className="text-blue-500 hover:underline cursor-pointer">
-            Go to World
-          </div>
-        </Link>
-      </div>
-    </div>
+    <>
+      {redirect(`/${Lang()}`)}
+    </>
   );
 }
