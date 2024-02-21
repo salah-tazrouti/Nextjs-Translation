77ab42d90db6721976bd990653865d72f6285229
stazrouti <TAZROUTI.SALAHEDDINE@ofppt-edu.ma>
2024-02-21 22:28:41 +0100

    dynamic title

--- src/pages/[lang]/Components/Navbar.js
+++ src/pages/[lang]/Components/Navbar.js
@@ -4,15 +4,16 @@ import React from 'react';
 import LanguageSwitcher from '@/app/LanguageSwitcher';
 import Link from 'next/link';
 import { Lang } from '@/app/Currentlng';
+import { useTranslation } from 'react-i18next';
 
 const Navbar = () => {
-  
+  const { t } = useTranslation();
     return (
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo or Brand */}
         <Link href={`/${Lang()}`} className="text-white text-xl font-bold">
-          IT Agency
+          {t('Head.title')}
         </Link>
         <div className="space-x-4">
             <LanguageSwitcher />
