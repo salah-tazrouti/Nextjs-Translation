d18db21f30c0b3cd80847d862dbfc33d3cc5170e
stazrouti <TAZROUTI.SALAHEDDINE@ofppt-edu.ma>
2024-03-01 21:02:34 +0100

    add tailwind animation

--- src/pages/[lang]/Components/Hero.js
+++ src/pages/[lang]/Components/Hero.js
@@ -8,14 +8,14 @@ export default function Hero() {
 
     return (
         <section className="hero-section text-center py-16">
-            <h1 className="text-4xl font-bold mb-4">{t('Home.title')}</h1>
-            <div className='flex'>
+            <h1 className="text-4xl font-bold mb-4 animate-jump-in">{t('Home.title')}</h1>
+            <div className='flex animate-fade-right'>
                 <div className='w-1/2'>
                     <img src='https://img.freepik.com/free-vector/top-view-dark-laptop-background-template_52683-6163.jpg?size=626&ext=jpg&ga=GA1.1.1573562918.1700557704&semt=ais'
                         className='h-96 object-cover rounded-lg shadow-lg m-2'
                     />
                 </div>
-                <div className='w-1/2 pt-24'>
+                <div className='w-1/2 pt-24 animate-fade-left'>
                     <p className="text-lg text-gray-600">{t('Home.welcome_section.0.title')}</p>
                     <p className="text-lg text-gray-600">{t('Home.welcome_section.0.description')}</p>
                     <p className="text-lg text-gray-600">{t('Home.welcome_section.0.title2')}</p>
