db830b85d35a72b3702c51e92d8b03e87d222616
stazrouti <TAZROUTI.SALAHEDDINE@ofppt-edu.ma>
2024-03-01 21:00:36 +0100

    add AOS

--- src/pages/[lang]/Components/About.js
+++ src/pages/[lang]/Components/About.js
@@ -1,12 +1,13 @@
 "use client";
 import { useTranslation } from "react-i18next";
+import 'aos/dist/aos.css'; 
 
 export default function Services() {
     const { t } = useTranslation();
     return (
-        <section id="About" className="about-section text-center py-4 m-40">
-            <h2 className="text-2xl font-bold mb-4">{t('Home.about_section.title')}</h2>
-            <div className="flex flex-col md:flex-row items-center justify-center space-y-4 md:space-y-0 md:space-x-8">
+        <section id="About" className="about-section text-center py-16 m-40">
+            <h2 className="text-2xl font-bold mb-4 " data-aos="fade-up">{t('Home.about_section.title')}</h2>
+            <div className="flex flex-col md:flex-row items-center justify-center space-y-4 md:space-y-0 md:space-x-8" data-aos="fade-up">
                 <div className="md:w-1/2">
                     <p className="text-gray-700">
                         {t('Home.about_section.description')}
