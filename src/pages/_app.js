7816691a476414b183391822798e9d5ed5ae7218
stazrouti <TAZROUTI.SALAHEDDINE@ofppt-edu.ma>
2024-03-01 20:59:56 +0100

    AOS init

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,10 +5,17 @@ import '@/app/globals.css'; // Import your global styles
 import Navbar from '@/pages/[lang]/Components/Navbar';
 import Head from 'next/head';
 import { useTranslation } from 'react-i18next';
+import { useEffect } from 'react';
+import AOS from 'aos';
 
 
 function MyApp({ Component, pageProps }) {
   const { t } = useTranslation();
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Set the default duration of animations
+    });
+  }, []);
   return (
     <div>
         <Head>
