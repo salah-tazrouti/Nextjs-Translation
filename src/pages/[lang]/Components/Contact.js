45ae435d0e8ec2dbcc956566e46004b2d1632048
stazrouti <TAZROUTI.SALAHEDDINE@ofppt-edu.ma>
2024-03-01 21:01:05 +0100

    add AOS

--- src/pages/[lang]/Components/Contact.js
+++ src/pages/[lang]/Components/Contact.js
@@ -7,16 +7,16 @@ export default function Services() {
     const { t } = useTranslation();
     return (
         <section id="Contact" className="contact-section text-center py-16">
-            <h2 className="text-2xl font-bold mb-4">{t('Home.contact_section.title')}</h2>
-            <div className="flex flex-col md:flex-row items-center justify-center space-y-4 md:space-y-0 md:space-x-8">
-                <div className="md:w-1/2">
+            <h2 className="text-2xl font-bold mb-4" data-aos="fade-up">{t('Home.contact_section.title')}</h2>
+            <div className="flex flex-col md:flex-row items-center justify-center space-y-4 md:space-y-0 md:space-x-8" >
+                <div className="ml-2 md:w-1/2" data-aos="fade-left">
                     <img
                         src="https://img.freepik.com/free-vector/flat-design-illustration-customer-support_23-2148887720.jpg?size=626&ext=jpg&ga=GA1.1.1573562918.1700557704&semt=sph"
                         alt="Customer Support"
                         className="rounded-md shadow-md w-96 ml-16"
                     />
                 </div>
-                <div className="md:w-1/2 text-left">
+                <div className="md:w-1/2 text-left" data-aos="fade-right">
                     <p className="text-gray-700">
                         {t('Home.contact_section.description')}
                     </p>
