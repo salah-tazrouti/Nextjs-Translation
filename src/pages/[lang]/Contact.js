3ad65230f0a902f0f1ce2b25bdf5ce0402ffddc7
stazrouti <TAZROUTI.SALAHEDDINE@ofppt-edu.ma>
2024-03-04 21:41:25 +0100

    add flex box

--- src/pages/[lang]/Contact.js
+++ src/pages/[lang]/Contact.js
@@ -28,62 +28,66 @@ const Contact = () => {
   };
 
   return (
-    <div className="contact-container bg-gray-100 p-4 mt-4">
-      <h2 className="text-2xl font-bold mb-4">{t('Contact.title')}</h2>
-      <p>{t('Contact.description')}</p>
+    <div className="bg-gray-100 p-4 mt-10 pt-10 flex flex-col md:flex-row h-screen">
+      <div className="flex-1 mt-32 md:pt-0 md:pr-4 animate-fade-left">
+        <h2 className="text-2xl font-bold mb-4">{t('Contact.title')}</h2>
+        <p>{t('Contact.description')}</p>
 
-      <div className="contact-info mt-4">
-        <p>{t('Contact.email')}: example@example.com</p>
-        <p>{t('Contact.phone')}: +1 123-456-7890</p>
+        <div className="contact-info mt-4">
+          <p>{t('Contact.email')}: example@example.com</p>
+          <p>{t('Contact.phone')}: +1 123-456-7890</p>
+        </div>
       </div>
+      <div className="flex-1 animate-fade-right">
+        <form onSubmit={handleSubmit} className="mt-4 p-4 bg-gray-100 rounded-md">
+          <label htmlFor="name" className="block text-base font-medium text-gray-700">
+            {t('Contact.form.0.name')}:
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+            className="mt-2 p-2 w-full border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+          />
 
-      <form onSubmit={handleSubmit} className="mt-4 p-4 bg-gray-100 rounded-md">
-        <label htmlFor="name" className="block text-base font-medium text-gray-700">
-          {t('Contact.form.0.name')}:
-        </label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          required
-          className="mt-2 p-2 w-full border rounded-md focus:outline-none focus:ring focus:border-blue-300"
-        />
+          <label htmlFor="email" className="block text-base font-medium text-gray-700 mt-4">
+            {t('Contact.form.0.email')}:
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+            className="mt-2 p-2 w-full border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+          />
 
-        <label htmlFor="email" className="block text-base font-medium text-gray-700 mt-4">
-          {t('Contact.form.0.email')}:
-        </label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-          className="mt-2 p-2 w-full border rounded-md focus:outline-none focus:ring focus:border-blue-300"
-        />
+          <label htmlFor="message" className="block text-base font-medium text-gray-700 mt-4">
+            {t('Contact.form.0.description')}:
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            required
+            className="mt-2 p-2 w-full border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+          ></textarea>
 
-        <label htmlFor="message" className="block text-base font-medium text-gray-700 mt-4">
-          {t('Contact.form.0.description')}:
-        </label>
-        <textarea
-          id="message"
-          name="message"
-          value={formData.message}
-          onChange={handleChange}
-          required
-          className="mt-2 p-2 w-full border rounded-md focus:outline-none focus:ring focus:border-blue-300"
-        ></textarea>
-
-        <button
-          type="submit"
-          className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-green-600 focus:outline-none focus:ring focus:border-green-300"
-        >
-          {t('Contact.form.0.button')}
-        </button>
-      </form>
+          <button
+            type="submit"
+            className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-700"
+          >
+            {t('Contact.form.0.button')}
+          </button>
+        </form>
+      </div>
     </div>
+
   );
 };
 
