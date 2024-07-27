da37f397adb7ddf1419d8b06d4f611e9b5e1cf86
stazrouti <TAZROUTI.SALAHEDDINE@ofppt-edu.ma>
2024-07-27 15:01:10 +0100

    enhance about design

--- src/pages/[lang]/Components/About.js
+++ src/pages/[lang]/Components/About.js
@@ -5,11 +5,13 @@ import 'aos/dist/aos.css';
 export default function Services() {
     const { t } = useTranslation();
     return (
-        <section id="About" className="about-section py-16 m-40 sm: m-5">
-            <h2 className="text-2xl font-bold mb-4 text-center" data-aos="fade-up">{t('Home.about_section.title')}</h2>
-            <div className="flex flex-col md:flex-row items-center justify-center space-y-4 md:space-y-0 md:space-x-8" data-aos="fade-up">
+        <section id="About" className="about-section py-16 px-6 lg:px-40 bg-gray-100">
+            <h2 className="text-3xl font-extrabold mb-6 text-center text-gray-800" data-aos="fade-up">
+                {t('Home.about_section.title')}
+            </h2>
+            <div className="flex flex-col md:flex-row items-center justify-between space-y-8 md:space-y-0 md:space-x-12" data-aos="fade-up">
                 <div className="md:w-1/2">
-                    <p className="text-gray-700">
+                    <p className="text-lg text-gray-600 leading-relaxed">
                         {t('Home.about_section.description')}
                     </p>
                 </div>
@@ -17,11 +19,10 @@ export default function Services() {
                     <img
                         src="https://img.freepik.com/free-vector/teamwork-concept-landing-page_52683-20165.jpg?size=626&ext=jpg&ga=GA1.1.1573562918.1700557704&semt=ais"
                         alt="Agency Team"
-                        className="rounded-md shadow-md w-96"
+                        className="rounded-lg shadow-lg w-full max-w-md"
                     />
                 </div>
-
             </div>
         </section>
-    )
+    );
 }
